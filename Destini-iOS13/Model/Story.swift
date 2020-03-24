b61126ae47f81d655647cba90a65d97d91d65eab
soumya-94 <soumya11207@gmail.com>
2020-03-25 00:47:34 +0530

    Step 3

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+struct Story {
+    
+    let title: String
+    let choice1: String
+    let choice2: String
+    
+    init(t: String, c1: String, c2: String)
+    {
+        title = t
+        choice1 = c1
+        choice2 = c2
+    }
+    
+}
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,7 @@
 
 import Foundation
 
-
+struct StoryBrain
+{
+    
+}
